95aa24414ecd513444c5a602b4df9bb812e5a3cf
Anjali200420 <159007704+Anjali200420@users.noreply.github.com>
2026-03-04 06:18:10 +0530

    Day-4(4th Mar, 26)  DailyQuestion

--- DailyProblems/findingKthBit(1545).cpp
+++ DailyProblems/findingKthBit(1545).cpp
@@ -1,42 +1,15 @@
 class Solution {
-private:
-    string reverseStr(string str){
-        string res = str;
-        reverse(res.begin(), res.end());
-
-        return res;
-    }
-
-    string invert(string str){
-        string new_str = "";
-
-        for(int i = 0; i < str.size(); i++){
-            if(str[i] == '0') new_str += '1';
-            else new_str += '0';
-        }
-
-        string ans = reverseStr(new_str);
-        return ans;
-    }
-
-    char creatingNthBinaryString(int n, int k){
-        string prev_str = "0";
-        string cur_str = "";
-        for(int i = 1; i < n; i++){
-           cur_str = prev_str + '1' + invert(prev_str);
-
-           prev_str = cur_str;
-        }
-
-        for(int i = 0; i < prev_str.size(); i++){
-            if(i == k-1) return prev_str[i];
-        }
-
-        return '-1';
-    }
-
 public:
     char findKthBit(int n, int k) {
-        return creatingNthBinaryString(n, k);
+        if(n == 1) return '0';
+
+        int len = (1 << n) - 1;
+        int mid = (len + 1)/2;
+
+        if(k == mid) return '1';
+        if(k < mid) return findKthBit(n-1, k);
+        
+        char c = findKthBit(n-1, len-k+1);
+        return c == '0' ? '1' : '0';
     }
 };
